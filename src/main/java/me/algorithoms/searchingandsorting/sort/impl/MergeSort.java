644692b23045a5c8231c330651acdd136ddf6d23
Prince Arora <prnc.arora0@gmail.com>
2019-05-20 16:38:39 +0530

    merge sort class

---
+++ src/main/java/me/algorithoms/searchingandsorting/sort/impl/MergeSort.java
@@ -0,0 +1,17 @@
+package me.algorithoms.searchingandsorting.sort.impl;
+
+import me.algorithoms.searchingandsorting.sort.Sort;
+
+import java.util.List;
+
+/**
+ * @author prince.arora
+ */
+public class MergeSort<T> extends Sort<T> {
+
+
+    @Override
+    public List<T> sort() {
+        return null;
+    }
+}
