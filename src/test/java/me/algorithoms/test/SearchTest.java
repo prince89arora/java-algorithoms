0c1cdc28a4c07001e3effbb56c8f50bb39903df4
Prince Arora <prnc.arora0@gmail.com>
2019-05-27 16:17:07 +0530

    binary search test with comparator

--- src/test/java/me/algorithoms/test/SearchTest.java
+++ src/test/java/me/algorithoms/test/SearchTest.java
@@ -3,19 +3,45 @@ package me.algorithoms.test;
 import me.algorithoms.searchingandsorting.search.Finder;
 import me.algorithoms.searchingandsorting.search.impl.BinerySearch;
 import me.algorithoms.searchingandsorting.search.impl.JumpSearch;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 /**
  * @author prince.arora
  */
 public class SearchTest {
 
+    IntegerComparator comparator;
+
+    @Before
+    public void before() {
+        this.comparator = new IntegerComparator();
+    }
+
+    private class IntegerComparator implements Comparator<Integer> {
+
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            if (o1 < o2)
+                return -1;
+            else if (o2 < o1)
+                return 1;
+            else
+                return 0;
+        }
+    }
+
     @Test
     public void binarySearchTest() {
-        Finder<Integer> finder = new BinerySearch<Integer>(Arrays.asList(new Integer[]{1,2,3,4,5,6,7,8,9}));
+        Finder<Integer> finder = new BinerySearch<Integer>(
+                Arrays.asList(new Integer[]{1,2,3,4,5,6,7,8,9}),
+                this.comparator
+        );
         Integer element = finder.find(7);
         assertEquals(element, new Integer(7));
     }
